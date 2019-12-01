764621a3baffe125dc2b3c2cd2c7465d0bb16276
KylinC <k1017856853@icloud.com>
2019-12-01 16:11:37 +0800

    modification added in README.log

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # EI312-Little-Car-Pro
 A repo of EI312, SJTU
+
+## Log
+> 11.30, a child page added.
+> 12.1, *direction control buttons added*, *handwriting route board added*.
--- Speech/app/src/main/java/com/example/speech/Mannual.java
+++ Speech/app/src/main/java/com/example/speech/Mannual.java
@@ -1,27 +1,37 @@
 package com.example.speech;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Mannual extends AppCompatActivity {
-
-    public static ImageView img;
-    private static Bitmap bitmap;
-
-    private Bluetooth bluetooth;
-    private Receive rev;
-    private SpeechControl.bitmapHandler handler;
-    static int num = 0;
-
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.key_layout);
-    };
+        final SignView signView=findViewById(R.id.signview);
+        final ImageView imageView=findViewById(R.id.img);
+        Button button=findViewById(R.id.btn);
+        Button button1=findViewById(R.id.btn2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageBitmap(signView.getBitmap());
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signView.clear();
+                imageView.setImageBitmap(null);
+            }
+        });
+    }
 }
